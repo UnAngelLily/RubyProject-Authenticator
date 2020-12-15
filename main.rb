ffd5271be2623eded95c5739afd21e492d3f13e7
UnAngelLily <unangellily@gmail.com>
2020-12-15 15:28:34 -0700

    started authenticator

---
+++ main.rb
@@ -0,0 +1,9 @@
+puts "Welcome to the Authenticator"
+25.times { print "-"}
+puts "This program will take input from the user and compare password\n
+      Username:"
+    username_1 = gets.chomp
+puts "Password:"
+    password_1 = gets.chomp
+
+if username_1 == " "
