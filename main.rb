c6a442d34aa49219577cf685574f82927fceaff7
UnAngelLily <unangellily@gmail.com>
2020-12-15 15:36:12 -0700

    finished if, elsif, and else conditionals

--- main.rb
+++ main.rb
@@ -6,4 +6,13 @@ puts "This program will take input from the user and compare password\n
 puts "Password:"
     password_1 = gets.chomp
 
-if username_1 == " "
+if username_1 == " " && password_1 == " "
+  puts "Username match\n
+    Welcome back, #{username_1}"
+elsif username_1 == "" && password_1 !== " "
+  puts "Username exist but the password is incorrect."
+elsif username_1 !== " "
+  puts "Username not found"
+else username_1 !== "" && password_1 !== " "
+  puts "No matching username or password."
+end   
