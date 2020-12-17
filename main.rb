f5cbc88cf4b205919e7f5f666f2dbfdb71c9e4a2
UnAngelLily <unangellily@gmail.com>
2020-12-17 13:18:30 -0700

    added more conditionals

--- main.rb
+++ main.rb
@@ -14,13 +14,16 @@ puts "This program will take input from the user and compare password\n
 puts "Password:"
     password_1 = gets.chomp
 
-if username_1 == " " && password_1 == " "
+if username_1 == "user1" && password_1 == "password1" ||
+  username_1 == "user2" && password_1 == "password2"
   puts "Username match\n
     Welcome back, #{username_1}"
 elsif username_1 == "" && password_1 !== " "
   puts "Credentials incorrect, login failed"
 elsif username_1 !== " "
   puts "Username not found"
-else username_1 !== "" && password_1 !== " "
+elsif  username_1 !== "" && password_1 !== " "
   puts "No matching username or password."
+else username
+  "Max amoung of login attempts have been made."
 end
