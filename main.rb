90eec5544ca17d53404815708458dd638f59d060
UnAngelLily <unangellily@gmail.com>
2020-12-17 11:58:18 -0700

    added usernames and passwords

--- main.rb
+++ main.rb
@@ -1,3 +1,11 @@
+users = [
+  {username: "user1", password: "password1" },
+  {username: "user2", password: "password2" },
+  {username: "user3", password: "password3" },
+  {username: "user4", password: "password4" },
+  {username: "user5", password: "password5" }
+]
+
 puts "Welcome to the Authenticator"
 25.times { print "-"}
 puts "This program will take input from the user and compare password\n
@@ -10,9 +18,9 @@ if username_1 == " " && password_1 == " "
   puts "Username match\n
     Welcome back, #{username_1}"
 elsif username_1 == "" && password_1 !== " "
-  puts "Username exist but the password is incorrect."
+  puts "Credentials incorrect, login failed"
 elsif username_1 !== " "
   puts "Username not found"
 else username_1 !== "" && password_1 !== " "
   puts "No matching username or password."
-end   
+end
